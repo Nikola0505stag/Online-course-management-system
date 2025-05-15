9bb68486a11443bb0585aa059745b7de3edc8ce8
Nikola Neychev <60015433+Nikola0505stag@users.noreply.github.com>
2025-05-15 13:00:06 +0300

    Working startup file

--- Homework/System.cpp
+++ Homework/System.cpp
@@ -1,29 +1,58 @@
 #include "System.h"
 
-
+// 1 - admin
+// 2 - student
+// 3 - teacher
 
 System::System()
 {
 	std::ifstream ifs("Users.txt");
 
+	int buffI;
 	char firstName[1024];
 	char lastName[1024];
 	char email[1024];
+	char password[1024];
 
-	ifs >> firstName;
-	ifs >> lastName;
-	ifs >> email;
+	while (!ifs.eof()) {
+		ifs >> buffI;
+		ifs >> firstName;
+		ifs >> lastName;
+		ifs >> email;
+		ifs >> password;
 
-	admin.setFirstName(firstName);
-	admin.setLastName(lastName);
-	admin.setEmail(email);
+		if (buffI == 1) {
+			admin.setFirstName(firstName);
+			admin.setLastName(lastName);
+			admin.setEmail(email);
+			admin.setPassword(password);
+		}
+		else if (buffI == 2) {
+			Student st(firstName, lastName, email, password);
+			students.push_back(st);
+		}
+		else {
+			Teacher th(firstName, lastName, email, password);
+			teachers.push_back(th);
+		}
+
+	}
 	ifs.close();
 
-	//std::cout << students.getCapacity();
-	//std::cout << teachers.getCapacity();
 }
 
 void System::print() const
 {
+	std::cout << "ADMIN: \n";
 	std::cout << admin;
+	std::cout << "\n\n";
+	std::cout << "TEACHERS: \n";
+	for (int i = 0; i < teachers.getSize(); i++) {
+		std::cout << teachers[i];
+	}
+	std::cout << "\n\n";
+	std::cout << "STUDENTS: \n";
+	for (int i = 0; i < students.getSize(); i++) {
+		std::cout << students[i];
+	}
 }
